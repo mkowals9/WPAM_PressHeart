a43bca6eaca68791aaec1d75b2e34eaf3bef52d3
Marcelina Anna Kowalska <m.a.kowalska@stud.elka.pw.edu.pl>
2021-05-01 23:28:49 +0200

    proper text on Hello + user login finally! :)

--- app/src/main/java/com/wpam/pressheart/fragments/MainLoggedMenuFragment.kt
+++ app/src/main/java/com/wpam/pressheart/fragments/MainLoggedMenuFragment.kt
@@ -1,5 +1,6 @@
 package com.wpam.pressheart.fragments
 
+import android.app.ActivityOptions
 import android.content.ContentValues.TAG
 import android.content.Intent
 import android.os.Bundle
@@ -7,6 +8,8 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.google.firebase.auth.FirebaseAuth
@@ -26,7 +29,6 @@ import kotlinx.android.synthetic.main.fragment_main_logged_menu.*
 class MainLoggedMenuFragment : Fragment() {
 
     private val db = Firebase.firestore
-    private var userLogin : String = "User"
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -40,34 +42,37 @@ class MainLoggedMenuFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
 
-        val userId : String = FirebaseAuth.getInstance().currentUser.uid.toString()
-        val docRef = db.collection("users_info").document(userId)
+        val userId : String = FirebaseAuth.getInstance().currentUser?.uid.toString()
+        Log.d(TAG, "userId: ${userId}")
+        val docRef = db.collection("Users_info").document(userId)
         docRef.get()
             .addOnSuccessListener {
                 document ->
                 if (document != null){
-                    print("userId: {userId}")
 
                     //userLogin= document.data.toString()
-                    this.userLogin = "whatever"
-                    print("userLogin : {userLogin}")
+                    val userLogin = document.data?.getValue("name")
+                    //Log.d(TAG, "${document.toString()}")
+                    Log.d(TAG, "Login: ${document.data?.getValue("name")}")
                     Log.d(TAG, "DocumentSnapshot data: ${document.data}")
-                    Log.d(TAG, "userLogin: ${this.userLogin}")
+                    view.findViewById<TextView>(R.id.textview_first).setText("Hello " + userLogin)
+
                 }
                 else
                 {
-
+                    view.findViewById<TextView>(R.id.textview_first).setText("Hello User")
                 }
             }
 
-        textview_first.text="Hello " + this.userLogin
         //view.findViewById<Button>(R.id.measurementsButton).setOnClickListener {
           //  findNavController().navigate(R.id.action_MainLoggedMenu_to_MeasurementsFragment)
         //}
         measurementsButton.setOnClickListener{
             //findNavController().navigate(R.id.action_MainLoggedMenu_to_MeasurementsFragment)
             val intent = Intent(this.activity as MainLoggedMenuActivity, MeasurementsActivity::class.java)
+
             startActivity(intent)
+            //(this.activity)?.overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
         }
 
         logOutButton.setOnClickListener{
